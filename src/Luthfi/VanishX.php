6563acebbc9d48adc995522d8d9092bfc3a97ad9
LuthMC <luthfi366822@gmail.com>
2024-09-29 20:32:41 +0700

    Add /vanishlist

--- src/Luthfi/VanishX.php
+++ src/Luthfi/VanishX.php
@@ -37,26 +37,30 @@ class BetterVanish extends PluginBase implements Listener {
     }
 
     public function setVanished(Player $player, bool $vanish): void {
-    $playerName = $player->getName();
+        $playerName = $player->getName();
 
-    if ($vanish) {
-        $this->vanishedPlayers[] = $playerName;
-        $player->setInvisible(true);
+        if ($vanish) {
+            $this->vanishedPlayers[] = $playerName;
+            $player->setInvisible(true);
 
-        $message = str_replace("{player}", $playerName, $this->getConfig()->get("notify-vanish-message", ""));
-    } else {
-        $this->vanishedPlayers = array_diff($this->vanishedPlayers, [$playerName]);
-        $player->setInvisible(false);
+            $message = str_replace("{player}", $playerName, $this->getConfig()->get("notify-vanish-message", ""));
+        } else {
+            $this->vanishedPlayers = array_diff($this->vanishedPlayers, [$playerName]);
+            $player->setInvisible(false);
 
-        $message = str_replace("{player}", $playerName, $this->getConfig()->get("notify-unvanish-message", ""));
-    }
+            $message = str_replace("{player}", $playerName, $this->getConfig()->get("notify-unvanish-message", ""));
+        }
 
-    foreach ($this->getServer()->getOnlinePlayers() as $onlinePlayer) {
-        if ($onlinePlayer->hasPermission("bettervanish.other")) {
-            $onlinePlayer->sendMessage($message);
+        foreach ($this->getServer()->getOnlinePlayers() as $onlinePlayer) {
+            if ($onlinePlayer->hasPermission("bettervanish.other")) {
+                $onlinePlayer->sendMessage($message);
+            }
         }
     }
-}
+
+    public function getVanishedPlayers(): array {
+        return $this->vanishedPlayers;
+    }
 
     public function onBlockBreak(BlockBreakEvent $event): void {
         if (!$this->getConfig()->get("break-block", true)) {
